d3c45fe124da9c4de86a415565221d4f0a67bc1e
premkuamr <kumarprem75715@gmail.com>
2024-09-12 11:14:53 +0530

    backend pushed

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 dotenv.config();
-const mongoose = require("mongoose")
-const cors = require("cors")
-app.use(cors())
-const userouter = require("./Router/routes.js")
+const mongoose = require("mongoose");
+const cors = require("cors");
+app.use(cors());
+const userouter = require("./Router/routes.js");
 app.use(express.json());
-mongoose.connect(process.env.DATABASE_URL)
-    .then(() => {
-        console.log("db connected successfully")
-    })
-    .catch(error => {
-        res.json({
-            message: error.message
-        })
-    })
-   
-    
-    
-    
-app.use("/",userouter)    
-    
-app.listen(3600, () => { 
-    console.log("server running");
-    
-})
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("db connected successfully");
+  })
+  .catch((error) => {
+    res.json({
+      message: error.message,
+    });
+  });
+
+app.use("/", userouter);
+
+app.listen(4000, () => {
+  console.log("server running");
+});
